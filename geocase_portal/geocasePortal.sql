3af25a11ff7c44fa6140a48b5e55955c040c1561
Glöckler, Falko <falko.gloeckler@mfn.berlin>
2019-10-18 09:06:35 +0200

    Some corrections and optimization in the installation script

--- geocase_portal/geocasePortal.sql
+++ geocase_portal/geocasePortal.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS geocase_portal;
+
 CREATE DATABASE IF NOT EXISTS geocase_portal CHARACTER SET utf8 COLLATE utf8_general_ci; 
 
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON geocase_portal.* TO 'geocase_user'@'%' IDENTIFIED BY 'yourDatabasePassword'; 
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 source install.cfg
 
-echo "Do you wish to remove an existing GeoCASe database container before installation?"
+echo "Do you wish to remove an existing GeoCASe database container (if installed) before installation?"
 select yn in "Yes" "No" "Cancel Installation"; do
     case $yn in
         Yes ) docker rm -f geocase_database; break;;
@@ -16,13 +16,13 @@ echo
 echo "Starting GeoCASe database ..."
 mkdir  -p $(pwd)/geocase_data
 chmod -R 777 $(pwd)/geocase_data
-docker run -d --restart always --name geocase_database -p $DOCKER_DATABASE_PORT:3306  --env "MYSQL_USER=$MYSQL_USER" --env "MYSQL_PASSWORD=$MYSQL_PASSWORD" --env "MYSQL_DATABASE=$MYSQL_DATABASE" --env "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" mariadb:latest
+docker run -d --restart always --name geocase_database -p $DOCKER_DATABASE_PORT:3306  --env "MYSQL_USER=$MYSQL_USER" --env "MYSQL_PASSWORD=$MYSQL_PASSWORD" --env "MYSQL_DATABASE=$MYSQL_BHIT_DATABASE" --env "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" mariadb:latest
 echo Waiting 30 seconds for the database server ...
 sleep 30
 echo
 echo
 
-echo "Do you wish to remove an existing BHIT container before installation?"
+echo "Do you wish to remove an existing BHIT container (if installed) before installation?"
 select yn in "Yes" "No" "Cancel Installation"; do
     case $yn in
         Yes ) docker rm -f geocase_bhit; break;;
@@ -33,7 +33,7 @@ done
 echo 
 echo 
 
-echo "Do you wish to remove an existing BHIT image before installation?"
+echo "Do you wish to remove an existing BHIT image (if installed) before installation?"
 select yn in "Yes" "No" "Cancel Installation"; do
     case $yn in
         Yes ) docker rmi geocase_bhit; break;;
@@ -57,17 +57,18 @@ echo
 
 echo "Starting BHIT ..."
 docker run -d --name geocase_bhit_tmp geocase_bhit
+echo Waiting 15 seconds for the BHIT server ...
 sleep 15s
 docker cp geocase_bhit_tmp:/tmp/bhit/schemaOnly.sql bhit/schemaOnly.sql
 docker cp bhit/schemaOnly.sql geocase_database:/tmp/schemaOnly.sql
 rm -f bhit/schemaOnly.sql
 docker rm -f geocase_bhit_tmp
-docker exec geocase_database sh -c "cat /tmp/schemaOnly.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE"
+docker exec geocase_database sh -c "cat /tmp/schemaOnly.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_BHIT_DATABASE"
 docker run -d --restart always --name geocase_bhit -p $DOCKER_BHIT_PORT:8080 --link geocase_database:geocase_database -v $(pwd)/bhit_data:/var/bhit/data geocase_bhit
 echo
 echo 
 
-echo "Do you wish to remove an existing GeoCASe Portal container before installation?"
+echo "Do you wish to remove an existing GeoCASe Portal container (if installed) before installation?"
 select yn in "Yes" "No" "Cancel Installation"; do
     case $yn in
         Yes ) docker rm -f geocase_portal; break;;
@@ -78,7 +79,7 @@ done
 echo 
 echo 
 
-echo "Do you wish to remove an existing GeoCASe Drupal 7 image before installation?"
+echo "Do you wish to remove an existing GeoCASe Drupal 7 image (if installed) before installation?"
 select yn in "Yes" "No" "Cancel Installation"; do
     case $yn in
         Yes ) docker rmi geocase_drupal; break;;
@@ -104,33 +105,29 @@ echo "Initiating Drupal 7 for GeoCASe Portal ..."
 docker run -d --name geocase_portal_tmp geocase_drupal
 docker cp geocase_portal_tmp:/var/www/html/sites/default $(pwd)/geocase_portal_sites/
 docker rm -f geocase_portal_tmp
+sleep 3s
 docker run -d --restart always  --name geocase_portal -p $DOCKER_GEOCASE_PORTAL_PORT:80 --link geocase_database:geocase_database -v $(pwd)/geocase_portal_sites:/var/www/html/sites geocase_drupal
 docker exec geocase_portal chown -R www-data:www-data /var/www/html/sites
-#docker exec geocase_portal sh -c "apt-get update && apt-get -y install unzip mysql-client && apt-get clean && rm -rf /var/lib/apt/lists/*"
-#echo
-#
-#echo "Installing drush command ..."
-#docker exec geocase_portal sh -c "cd /var/www/ && curl -sS https://getcomposer.org/installer | php"
-#docker exec geocase_portal sh -c "ln -s /usr/local/bin/composer /usr/bin/composer"
-#docker exec geocase_portal sh -c "curl -sL https://github.com/drush-ops/drush/archive/8.3.0.zip > /var/www/drush_8_3_0.zip"
-#docker exec -it geocase_portal sh -c "cd /var/www/ && unzip -q drush_8_3_0.zip"
-#docker exec -it geocase_portal sh -c "mv /var/www/drush-8.3.0 /usr/local/src/drush && rm -R /var/www/drush_8_3_0.zip"
-#docker exec -it geocase_portal sh -c "cd /usr/local/src/drush/ && php /var/www/composer.phar install"
-#docker exec -it geocase_portal sh -c "ln -s /usr/local/src/drush/drush /usr/bin/drush"
-#echo
-#
-#echo "Testing drush ..."
-#docker exec geocase_portal drush --version
 echo
 echo
 
 echo "Installing GeoCASe's database structures ..."
-docker cp geocase_portal/geocasePortal.sql geocase_database:/tmp/geocasePortal.sql
-docker cp geocase_portal/geocase_bhit.sql geocase_database:/tmp/geocase_bhit.sql
-docker exec geocase_database sh -c "cat /tmp/geocasePortal.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE"
-docker exec geocase_database sh -c "cat /tmp/geocase_bhit.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE"
+echo "Do you agree to re-create an existing GeoCASe database?"
+select yn in "Yes" "No" "Cancel Installation"; do
+    case $yn in
+        Yes ) 
+		docker cp geocase_portal/geocasePortal.sql geocase_database:/tmp/geocasePortal.sql;
+		docker cp geocase_portal/geocase_bhit.sql geocase_database:/tmp/geocase_bhit.sql;
+		docker exec geocase_database sh -c "cat /tmp/geocasePortal.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_GEOCASE_DATABASE";
+		docker exec geocase_database sh -c "cat /tmp/geocase_bhit.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_GEOCASE_DATABASE";
+		break;;
+        No ) break;;
+        "Cancel Installation" ) exit;;
+    esac
+done
+echo 
 echo
-echo
+
 echo "Installing GeoCASe Portal ..."
 sleep 10s
 docker exec -it geocase_portal drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@geocase_database:3306/geocase_portal
@@ -138,7 +135,8 @@ echo
 echo "If the installation was successful, please note the admin password from the lines above."
 read -p "Pressing any key to continue..."
 
-echo "\$databases['bhit'] = array ( 'default' => array ('database' => '$MYSQL_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
+echo "\$databases['default'] = array ( 'default' => array ('database' => '$MYSQL_GEOCASE_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
+echo "\$databases['bhit'] = array ( 'default' => array ('database' => '$MYSQL_BHIT_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
 
 docker exec -it geocase_portal drush dl -y views views_database_connector webform
 docker exec -it geocase_portal drush en -y views views_ui views_database_connector webform
