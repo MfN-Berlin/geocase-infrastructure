a2ac66c235f73583b5bc52b7202fb2b30ca05de2
Glöckler, Falko <falko.gloeckler@mfn.berlin>
2019-11-11 17:36:06 +0100

    Optimization of the install script

---
+++ initial_portal_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing GeoCASe Portal ..."
+sleep 10s
+docker exec -it geocase_portal sh -c "echo \"ini_set('memory_limit', '512M');\" >> /var/www/html/drush/drushrc.php"
+read -p "Pressing any key to continue..."
+docker restart geocase_portal geocase_database
+docker exec -it geocase_portal drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@geocase_database:$DOCKER_DATABASE_PORT/geocase_portal
+echo
+echo "If the installation was successful, please note the admin password from the lines above."
+read -p "Pressing any key to continue..."
+
+echo "\$databases['default'] = array ( 'default' => array ('database' => '$MYSQL_GEOCASE_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
+echo "\$databases['bhit'] = array ( 'default' => array ('database' => '$MYSQL_BHIT_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
+
+docker exec -it geocase_portal drush dl -y views views_database_connector webform
+docker exec -it geocase_portal drush en -y views views_ui views_database_connector webform
+docker exec -it geocase_portal drush cc all
+
+echo
+echo 
+
+echo Done!
+echo BHIT at http://localhost:$DOCKER_BHIT_PORT/bhit/
+echo GeoCASe Portal at http://localhost:$DOCKER_GEOCASE_PORTAL_PORT/
+echo 
+echo Please manually import the BHIT-GeoCASe Drupal View at http://localhost:$DOCKER_GEOCASE_PORTAL_PORT/localhost/admin/structure/views/import
+
--- install.sh
+++ install.sh
@@ -118,8 +118,8 @@ select yn in "Yes" "No" "Cancel Installation"; do
         Yes ) 
 		docker cp geocase_portal/geocasePortal.sql geocase_database:/tmp/geocasePortal.sql;
 		docker cp geocase_portal/geocase_bhit.sql geocase_database:/tmp/geocase_bhit.sql;
-		docker exec geocase_database sh -c "cat /tmp/geocasePortal.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_GEOCASE_DATABASE";
-		docker exec geocase_database sh -c "cat /tmp/geocase_bhit.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_GEOCASE_DATABASE";
+		docker exec geocase_database sh -c "cat /tmp/geocasePortal.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD";
+		docker exec geocase_database sh -c "cat /tmp/geocase_bhit.sql | mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_BHIT_DATABASE";
 		break;;
         No ) break;;
         "Cancel Installation" ) exit;;
@@ -128,25 +128,4 @@ done
 echo 
 echo
 
-echo "Installing GeoCASe Portal ..."
-sleep 10s
-docker exec -it geocase_portal drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@geocase_database:3306/geocase_portal
-echo
-echo "If the installation was successful, please note the admin password from the lines above."
-read -p "Pressing any key to continue..."
-
-echo "\$databases['default'] = array ( 'default' => array ('database' => '$MYSQL_GEOCASE_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
-echo "\$databases['bhit'] = array ( 'default' => array ('database' => '$MYSQL_BHIT_DATABASE', 'username' => '$MYSQL_USER', 'password' => '$MYSQL_PASSWORD', 'host' => 'geocase_database', 'port' => '', 'driver' => 'mysql', 'prefix' => '',));" >> geocase_portal_sites/default/settings.php
-
-docker exec -it geocase_portal drush dl -y views views_database_connector webform
-docker exec -it geocase_portal drush en -y views views_ui views_database_connector webform
-docker exec -it geocase_portal drush cc all
-
-echo
-echo 
-
-echo Done!
-echo BHIT at http://localhost:$DOCKER_BHIT_PORT/bhit/
-echo GeoCASe Portal at http://localhost:$DOCKER_GEOCASE_PORTAL_PORT/
-echo 
-echo Please manually import the BHIT-GeoCASe Drupal View at http://localhost:$DOCKER_GEOCASE_PORTAL_PORT/localhost/admin/structure/views/import
+source initial_portal_setup.sh
